9cee00b296765c7a241a1661aecb41f1bb0d6f2c
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-20 20:49:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,22 @@
 class Application
-
+  @@items = []
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item = req.path.split("/items/").last
+      search_term = req.path.split("/items/").last
+      @@items.each do |item|
+        search_term = item if item.name == search_term
+      end
+      if (search_term.is_a? Item)
+        resp.write "The item #{search_term.name} costs #{search-term.price}"
+        resp.status = 200
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end 
     else
       resp.write "Route not found"
       resp.status = 404
